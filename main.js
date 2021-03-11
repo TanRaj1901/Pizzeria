ba362a4ee9d8aad754e373fa5518465f3eb2d423
TanRaj1901 <75247451+TanRaj1901@users.noreply.github.com>
2021-03-11 10:45:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,5 +12,14 @@ function getmenu() {
 }
 
 function add_item() {
-    
+    var htmldata;
+    var item = document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata = "<section class='cards'>"
+    for (var i = 0 ; i < menu_list_array.length ; i++) {
+        htmldata = htmldata + '<div class="card">' + "<image src='images/pizzaImg.png'/>" + menu_list_array[i] + '</div>'
+    }
+    htmldata=htmldata + "</section>"
+    document.getElementById("display_addedmenu").innerHTML = htmldata;
 }
